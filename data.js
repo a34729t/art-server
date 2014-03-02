2ea653becbe6f436eb193568a06b5184ac381a77
Nicolas Flacco <ncf1989@yahoo.com>
2014-03-01 18:37:41 -0800

    foo

---
+++ data.js
@@ -0,0 +1,79 @@
+var artists = {
+    0: {
+        "artist_name":null,
+        "artist_bio": null,
+        "artist_interview":null,
+        "aritst_alt_text":null,
+        "artist_preview_image":null
+    },
+    1: {
+        "artist_name":null,
+        "artist_bio": null,
+        "artist_interview":null,
+        "aritst_alt_text":null,
+        "artist_preview_image":null
+    },
+    2: {
+        "artist_name":null,
+        "artist_bio": null,
+        "artist_interview":null,
+        "aritst_alt_text":null,
+        "artist_preview_image":null
+    }
+};
+
+var works = {
+    0: {
+        "artist": artists[0],
+        "title": null,
+        "caption": null,
+        "description": null,
+        "image": null
+    },
+    1: {
+        "artist": artists[0],
+        "title": null,
+        "caption": null,
+        "description": null,
+        "image": null
+    },
+    2: {
+        "artist": artists[1],
+        "title": null,
+        "caption": null,
+        "description": null,
+        "image": null
+    },
+    3: {
+        "artist": artists[1],
+        "title": null,
+        "caption": null,
+        "description": null,
+        "image": null
+    },
+    4: {
+        "artist": artists[2],
+        "title": null,
+        "caption": null,
+        "description": null,
+        "image": null
+    },
+    5: {
+        "artist": artists[2],
+        "title": null,
+        "caption": null,
+        "description": null,
+        "image": null
+    }
+};
+
+var beacon2art = {
+    "1507110507": works[0],
+    "4556564072": works[1],
+    "6046356367": works[2],
+    "4967229621": works[3],
+    "54450962": works[4],
+    "236807349": works[5]
+};
+
+exports.beacon2art = beacon2art;
--- web.js
+++ web.js
@@ -1,33 +1,28 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
-var app = express();
+var data = require('./data');
 
-var data = {
-    1507110507: {},
-    "1507110507": {},
-    "4556564072": {},
-    "6046356367": {},
-    "4967229621": {},
-    "54450962": {},
-    "236807349": {}
-};
+var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.param('id', /^\d+$/);
+app.get("/:id([0-9]+)", function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    
+    console.log("in id route");
+    console.log(req.params.id);
+    
+    if (req.params.id in data.beacon2art) {
+        var result = data.beacon2art[req.params.id]
+        res.send(JSON.stringify(result));
+    } else {
+        res.send(JSON.stringify({err : "not found"}));
+    }
+});
 
 app.get('/', function(req, res) {
     res.send('Hello World!');
-});
-
-app.get(':id', function(req, res) {
-    console.log("in id route");
-    console.log(id);
-    var result = data[id]
-    
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(result));
 });
 
 var port = Number(process.env.PORT || 5000);
