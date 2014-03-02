3c27b5fc3f64a06b7994b260637bbfa1e0b31840
Nicolas Flacco <ncf1989@yahoo.com>
2014-03-01 17:32:50 -0800

    i hope this works

--- web.js
+++ web.js
@@ -3,15 +3,26 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
+var data = {
+    "1507110507": {},
+    "4556564072": {},
+    "6046356367": {},
+    "4967229621": {},
+    "54450962": {},
+    "236807349": {}
+};
+
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
-app.get('/^\d+$/', function(req, res) {
+app.get(':id(/^\d+$/)', function(req, res) {
+    var result = data[id]
+    
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ a: 1, b: 2 }));
+    res.end(JSON.stringify(result));
 });
 
 var port = Number(process.env.PORT || 5000);
