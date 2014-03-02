4e49be7ad8df24fea33d9f8a2b9d2f802c6eb2e4
Nicolas Flacco <ncf1989@yahoo.com>
2014-03-01 17:28:34 -0800

    added json route

--- web.js
+++ web.js
@@ -9,6 +9,11 @@ app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
+app.get('/^\d+$/', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ a: 1, b: 2 }));
+});
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
     console.log("Listening on " + port);
